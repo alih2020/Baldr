745ad41e1e5e38cb2ddb58ff22d4b9ddaa623126
Hamza Ali <hamzaalih@hotmail.com>
2022-03-03 15:55:26 -0500

    commit

---
+++ target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ca.qc.bdeb.baldr.formattage"},{"l":"ca.qc.bdeb.baldr.ihm"},{"l":"ca.qc.bdeb.baldr.ihm.fileFilters"},{"l":"ca.qc.bdeb.baldr.ihm.renderers"},{"l":"ca.qc.bdeb.baldr.ihm.renderers.utils"},{"l":"ca.qc.bdeb.baldr.main"},{"l":"ca.qc.bdeb.baldr.noyau"},{"l":"ca.qc.bdeb.baldr.utils"}];updateSearchResults();
